f45963fa5156bbdc16b305b8ecc184ab63c8cbe1
sofiamarancibia <sofiamarancibia@gmail.com>
2022-05-23 16:50:15 -0300

    hasta foto de perfil

--- ProyectoIntegradorFinalArgProgSMArancibia/src/app/app.module.ts
+++ ProyectoIntegradorFinalArgProgSMArancibia/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
